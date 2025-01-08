ed7a82fa7e0b62af039b9a2e25602251c50c7ad3
SanikaWadake <sanika.wadake@systems-plus.com>
2025-01-08 10:37:40 +0530

    SFRA-1-GiftCertificate

---
+++ cartridges/custom_cartridge_master/cartridges/custom_sanika1/cartridge/client/default/js/product/detail.js
@@ -0,0 +1,136 @@
+'use strict';
+
+ var base = require('base/product/detail');
+ var base1 = require('./base');
+ //var base1 = require('base/product/base');
+
+/**
+ * Enable/disable UI elements
+ * @param {boolean} enableOrDisable - true or false
+ */
+
+function updateGiftCerticate()
+{
+    $('body').on('change', 'input[name="giftCertificate"]', function (event) 
+    {
+        const $paperRadio = $('#paperGiftCertificate');
+        const $personalizeButton = $('#personalizeButton');
+        const $addToCart=$('#add-to-cart');
+
+        if ($paperRadio.is(':checked')) {
+            
+            $personalizeButton.hide(); 
+            $addToCart.show();
+        }
+         else if ($('#emailGiftCertificate').is(':checked')){
+            
+            $personalizeButton.show();
+            $addToCart.hide(); 
+            
+        }
+        localStorage.setItem('giftCertificateType', $(this).val());
+    });
+
+    // Initial check to set the button state on page load
+    if ($('#paperGiftCertificate').is(':checked')) {
+        $('#personalizeButton').hide();
+    } else {
+        $('#personalizeButton').show();
+    }
+     // Restore saved state from localStorage
+    $(document).ready(function () {
+        const savedType = localStorage.getItem('giftCertificateType');
+        if (savedType) {
+            $(`#emailGiftCertificate[value="${savedType}"]`).prop('checked', true);
+        }
+    });
+
+
+}
+
+function updateAddToCart () {
+    $('body').on('product:updateAddToCart', function (e, response) {
+        $('button.add-to-cart', response.$productContainer).attr(
+            'disabled',
+            (!response.product.readyToOrder || !response.product.available)
+        );
+        $('#personalizeButton').attr(
+            'disabled',
+            (!response.product.readyToOrder || !response.product.available)
+        );
+        var enable = $('.product-availability').toArray().every(function (item) {
+            return $(item).data('available') && $(item).data('ready-to-order');
+        });
+        module.exports.methods.updateAddToCartEnableDisableOtherElements(!enable);
+        $('#personalizeButton').attr('disabled', !enable);
+    });
+}
+
+function closeModal()
+{
+    $('button.add-to-cart').on('click', function (e) {
+        e.preventDefault(); 
+        $('#chooseProductModal').modal('hide'); 
+    });
+    
+}
+
+function emailValidation()
+{
+        $(document).ready(function() {
+            const emailInput = $('#email');
+            const emailError = $('#emailError');
+            const addToCartButton = $('#addToCart');
+            const firstNameInput = $('#firstName');
+            const lastNameInput = $('#lastName');
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    
+            emailError.hide();
+            addToCartButton.prop('disabled', true); // Disable button initially
+
+            $('#chooseProductModal').on('show.bs.modal', function () {
+                addToCartButton.prop('disabled', true);
+                emailInput.val('');  
+                firstNameInput.val('');  
+                lastNameInput.val('');  
+                emailError.hide(); 
+            });
+    
+            function validateInputs() {
+              
+                if (
+                    firstNameInput.val().trim() !== '' &&
+                    lastNameInput.val().trim() !== '' &&
+                    emailRegex.test(emailInput.val())
+                ) {
+                    addToCartButton.prop('disabled', false);
+                } else {
+                    addToCartButton.prop('disabled', true);
+                }
+            }
+            firstNameInput.on('input', validateInputs);
+            lastNameInput.on('input', validateInputs);
+            emailInput.on('input', function() {
+              
+                emailError.toggle(!emailRegex.test(emailInput.val())); 
+                validateInputs(); 
+            });
+    
+            $('body').on('click', '#addToCart', function (e) {
+                e.preventDefault();
+    
+                if (emailRegex.test(emailInput.val())) {
+                    console.log('Valid email:', emailInput.val());
+                    
+                }
+            });
+        });   
+}
+base.addToCart= base1.addToCart;
+base.updateGiftCerticate=updateGiftCerticate;
+base.closeModal=closeModal;
+base.updateAddToCart=updateAddToCart;
+base.emailValidation=emailValidation;
+module.exports=base;
+
+
---
+++ cartridges/custom_cartridge_master/cartridges/custom_sanika1/cartridge/controllers/Cart.js
@@ -0,0 +1,159 @@
+'use strict';
+
+/**
+ * @namespace Cart
+ */
+var server = require('server');
+server.extend(module.superModule);
+server.replace('AddProduct', function (req, res, next) {
+    var BasketMgr = require('dw/order/BasketMgr');
+    var Resource = require('dw/web/Resource');
+    var URLUtils = require('dw/web/URLUtils');
+    var Transaction = require('dw/system/Transaction');
+    var CartModel = require('*/cartridge/models/cart');
+    var ProductLineItemsModel = require('*/cartridge/models/productLineItems');
+    var cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
+    var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
+
+    var currentBasket = BasketMgr.getCurrentOrNewBasket();
+    var previousBonusDiscountLineItems = currentBasket.getBonusDiscountLineItems();
+    var productId = req.form.pid;
+    var isGiftCertificate = req.form.isGiftCertificate; 
+    var giftCertificateType = req.form.giftCertificateType || null;
+    var firstName=req.form.firstName;
+    var lastName=req.form.lastName;
+    var email=req.form.email;
+
+    var childProducts = Object.hasOwnProperty.call(req.form, 'childProducts')
+        ? JSON.parse(req.form.childProducts)
+        : [];
+    var options = req.form.options ? JSON.parse(req.form.options) : [];
+    var quantity;
+    var result;
+    var pidsObj;
+
+    if (currentBasket) {
+        Transaction.wrap(function () {
+            if (!req.form.pidsObj) {
+                quantity = parseInt(req.form.quantity, 10);
+                result = cartHelper.addProductToCart(
+                    currentBasket,
+                    productId,
+                    quantity,
+                    childProducts,
+                    options,
+                    firstName,
+                    lastName,
+                    email,
+                    isGiftCertificate, //true
+                    giftCertificateType //email
+                );
+            } else {
+                // product set
+                pidsObj = JSON.parse(req.form.pidsObj);
+                result = {
+                    error: false,
+                    message: Resource.msg('text.alert.addedtobasket', 'product', null)
+                };
+
+                pidsObj.forEach(function (PIDObj) {
+                    quantity = parseInt(PIDObj.qty, 10);
+                    var pidOptions = PIDObj.options ? JSON.parse(PIDObj.options) : {};
+                    var PIDObjResult = cartHelper.addProductToCart(
+                        currentBasket,
+                        PIDObj.pid,
+                        quantity,
+                        childProducts,
+                        pidOptions,
+                        firstName,
+                        lastName,
+                        email
+                    );
+                    if (PIDObjResult.error) {
+                        result.error = PIDObjResult.error;
+                        result.message = PIDObjResult.message;
+                    }
+                });
+            }
+            if (!result.error) {
+                cartHelper.ensureAllShipmentsHaveMethods(currentBasket);
+                basketCalculationHelpers.calculateTotals(currentBasket);
+            }
+        });
+    }
+    var quantityTotal = ProductLineItemsModel.getTotalQuantity(currentBasket.productLineItems);
+    var cartModel = new CartModel(currentBasket);
+    var urlObject = {
+        url: URLUtils.url('Cart-ChooseBonusProducts').toString(),
+        configureProductstUrl: URLUtils.url('Product-ShowBonusProducts').toString(),
+        addToCartUrl: URLUtils.url('Cart-AddBonusProducts').toString()
+    };
+    var newBonusDiscountLineItem = cartHelper.getNewBonusDiscountLineItem(
+        currentBasket,
+        previousBonusDiscountLineItems,
+        urlObject,
+        result.uuid
+    );
+    if (newBonusDiscountLineItem) {
+        var allLineItems = currentBasket.allProductLineItems;
+        var collections = require('*/cartridge/scripts/util/collections');
+        collections.forEach(allLineItems, function (pli) {
+            if (pli.UUID === result.uuid) {
+                Transaction.wrap(function () {
+                    pli.custom.bonusProductLineItemUUID = 'bonus'; // eslint-disable-line no-param-reassign
+                    pli.custom.preOrderUUID = pli.UUID; // eslint-disable-line no-param-reassign
+                });
+            }
+        });
+    }
+
+    var reportingURL = cartHelper.getReportingUrlAddToCart(currentBasket, result.error);
+    res.json({
+        reportingURL: reportingURL,
+        quantityTotal: quantityTotal,
+        message: result.message,
+        cart: cartModel,
+        newBonusDiscountLineItem: newBonusDiscountLineItem || {},
+        error: result.error,
+        pliUUID: result.uuid,
+        minicartCountOfItems: Resource.msgf('minicart.count', 'common', null, quantityTotal)
+    });
+
+    next();
+});
+server.replace(
+    'Show',
+    function (req, res, next) {
+        var BasketMgr = require('dw/order/BasketMgr');
+        var Transaction = require('dw/system/Transaction');
+        var CartModel = require('*/cartridge/models/cart');
+        var cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
+        var reportingUrlsHelper = require('*/cartridge/scripts/reportingUrls');
+        var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
+
+        var currentBasket = BasketMgr.getCurrentBasket();
+        var reportingURLs;
+
+        if (currentBasket) {
+            Transaction.wrap(function () {
+                if (currentBasket.currencyCode !== req.session.currency.currencyCode) {
+                    currentBasket.updateCurrency();
+                }
+                cartHelper.ensureAllShipmentsHaveMethods(currentBasket);
+
+                basketCalculationHelpers.calculateTotals(currentBasket);
+            });
+        }
+
+        if (currentBasket && currentBasket.allLineItems.length) {
+            reportingURLs = reportingUrlsHelper.getBasketOpenReportingURLs(currentBasket);
+        }
+
+        res.setViewData({ reportingURLs: reportingURLs });
+
+        var basketModel = new CartModel(currentBasket);
+        res.render('cart/cart', basketModel);
+        next();
+    }
+);
+module.exports = server.exports();
