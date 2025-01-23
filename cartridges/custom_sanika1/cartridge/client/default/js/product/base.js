9890d57aaa07357a716325a9b989f49f4d3abed9
SanikaWadake <sanika.wadake@systems-plus.com>
2025-01-23 13:01:24 +0530

    SFRA-1|PR-Changes

--- cartridges/custom_sanika1/cartridge/client/default/js/product/base.js
+++ cartridges/custom_sanika1/cartridge/client/default/js/product/base.js
@@ -1,9 +1,11 @@
 'use strict';
 
- var base1 = require('base/product/base');
- function getAddToCartUrl() {
+var base = require('base/product/base');
+
+function getAddToCartUrl() {
     return $('.add-to-cart-url').val();
 }
+
 function getChildProducts() {
     var childProducts = [];
     $('.bundle-item').each(function () {
@@ -15,18 +17,17 @@ function getChildProducts() {
 
     return childProducts.length ? JSON.stringify(childProducts) : [];
 }
+
 function handlePostCartAdd(response) {
     $('.minicart').trigger('count:update', response);
     var messageType = response.error ? 'alert-danger' : 'alert-success';
-    // show add to cart toast
-    if (response.newBonusDiscountLineItem
-        && Object.keys(response.newBonusDiscountLineItem).length !== 0) {
+
+    // Show add to cart toast
+    if (response.newBonusDiscountLineItem && Object.keys(response.newBonusDiscountLineItem).length !== 0) {
         chooseBonusProducts(response.newBonusDiscountLineItem);
     } else {
         if ($('.add-to-cart-messages').length === 0) {
-            $('body').append(
-                '<div class="add-to-cart-messages"></div>'
-            );
+            $('body').append('<div class="add-to-cart-messages"></div>');
         }
         $('.add-to-cart-messages').append(
             '<div class="alert ' + messageType + ' add-to-basket-alert text-center" role="alert">'
@@ -45,8 +46,7 @@ function getOptions($productContainer) {
         .map(function () {
             var $elOption = $(this).find('.options-select');
             var urlValue = $elOption.val();
-            var selectedValueId = $elOption.find('option[value="' + urlValue + '"]')
-                .data('value-id');
+            var selectedValueId = $elOption.find('option[value="' + urlValue + '"]').data('value-id');
             return {
                 optionId: $(this).data('option-id'),
                 selectedValueId: selectedValueId
@@ -55,13 +55,15 @@ function getOptions($productContainer) {
     return JSON.stringify(options);
 }
 
- function addToCart() {
+function addToCart() {
     $(document).on('click', 'button.add-to-cart, button.add-to-cart-global', function () {
         var addToCartUrl;
         var pid;
         var pidsObj;
         var setPids;
+
         $('body').trigger('product:beforeAddToCart', this);
+
         if ($('.set-items').length && $(this).hasClass('add-to-cart-global')) {
             setPids = [];
             $('.product-detail').each(function () {
@@ -75,20 +77,20 @@ function getOptions($productContainer) {
             });
             pidsObj = JSON.stringify(setPids);
         }
-        if($('.wishlistpage').length)
-        {
-          pid=$(this).closest('.product-detail').data('pid');
-        }
-        else
-        {
-            pid = base1.getPidValue($(this));
+
+        if ($('.wishlistpage').length) {
+            pid = $(this).closest('.product-detail').data('pid');
+        } else {
+            pid = base.getPidValue($(this));
         }
 
         var $productContainer = $(this).closest('.product-detail');
         if (!$productContainer.length) {
             $productContainer = $(this).closest('.quick-view-dialog').find('.product-detail');
         }
+
         addToCartUrl = getAddToCartUrl();
+
         var isGiftCertificate = $productContainer.find('.gift-certificate-options').length > 0;
         var giftCertificateType = null;
         if (isGiftCertificate) {
@@ -96,23 +98,28 @@ function getOptions($productContainer) {
                 .find('input[name="giftCertificate"]:checked')
                 .val();
         }
+
         var form = {
             pid: pid,
             pidsObj: pidsObj,
             childProducts: getChildProducts(),
-            quantity: base1.getQuantitySelected($(this))
+            quantity: base.getQuantitySelected($(this))
         };
+
         if (isGiftCertificate) {
-           form.giftCertificateType = giftCertificateType || null;
-           form.isGiftCertificate=true;
-           form.firstName = $('#firstName').val() || null;
-           form.lastName = $('#lastName').val() || null;
-           form.email = $('#email').val() || null;
+            form.giftCertificateType = giftCertificateType || null;
+            form.isGiftCertificate = true;
+            form.firstName = $('#firstName').val() || null;
+            form.lastName = $('#lastName').val() || null;
+            form.email = $('#email').val() || null;
         }
+
         if (!$('.bundle-item').length) {
             form.options = getOptions($productContainer);
         }
+
         $(this).trigger('updateAddToCartFormData', form);
+
         if (addToCartUrl) {
             $.ajax({
                 url: addToCartUrl,
@@ -132,5 +139,5 @@ function getOptions($productContainer) {
     });
 }
 
-base1.addToCart=addToCart;
-module.exports=base1;
+base.addToCart = addToCart;
+module.exports = base;
--- cartridges/custom_sanika1/cartridge/client/default/js/product/detail.js
+++ cartridges/custom_sanika1/cartridge/client/default/js/product/detail.js
@@ -1,32 +1,24 @@
 'use strict';
 
- var base = require('base/product/detail');
- var base1 = require('./base');
+var base = require('base/product/detail');
 
 /**
  * Enable/disable UI elements
- * @param {boolean} enableOrDisable - true or false
  */
-
-function updateGiftCerticate()
-{
-    $('body').on('change', 'input[name="giftCertificate"]', function (event)
-    {
+function updateGiftCerticate() {
+    $('body').on('change', 'input[name="giftCertificate"]', function (event) {
         const $paperRadio = $('#paperGiftCertificate');
         const $personalizeButton = $('#personalizeButton');
-        const $addToCart=$('#add-to-cart');
+        const $addToCart = $('#add-to-cart');
 
         if ($paperRadio.is(':checked')) {
-
             $personalizeButton.hide();
             $addToCart.show();
-        }
-         else if ($('#emailGiftCertificate').is(':checked')){
-
+        } else if ($('#emailGiftCertificate').is(':checked')) {
             $personalizeButton.show();
             $addToCart.hide();
-
         }
+
         localStorage.setItem('giftCertificateType', $(this).val());
     });
 
@@ -36,98 +28,96 @@ function updateGiftCerticate()
     } else {
         $('#personalizeButton').show();
     }
-     // Restore saved state from localStorage
+
+    // Restore saved state from localStorage
     $(document).ready(function () {
         const savedType = localStorage.getItem('giftCertificateType');
         if (savedType) {
             $(`#emailGiftCertificate[value="${savedType}"]`).prop('checked', true);
         }
     });
-
-
 }
-function updateAddToCart () {
+
+function updateAddToCart() {
     $('body').on('product:updateAddToCart', function (e, response) {
         $('button.add-to-cart', response.$productContainer).attr(
             'disabled',
             (!response.product.readyToOrder || !response.product.available)
         );
+
         $('#personalizeButton').attr(
             'disabled',
             (!response.product.readyToOrder || !response.product.available)
         );
+
         var enable = $('.product-availability').toArray().every(function (item) {
             return $(item).data('available') && $(item).data('ready-to-order');
         });
+
         module.exports.methods.updateAddToCartEnableDisableOtherElements(!enable);
         $('#personalizeButton').attr('disabled', !enable);
     });
 }
-function closeModal()
-{
+
+function closeModal() {
     $('button.add-to-cart').on('click', function (e) {
         e.preventDefault();
         $('#chooseProductModal').modal('hide');
     });
-
 }
-function emailValidation()
-{
-        $(document).ready(function() {
-            const emailInput = $('#email');
-            const emailError = $('#emailError');
-            const addToCartButton = $('#addToCart');
-            const firstNameInput = $('#firstName');
-            const lastNameInput = $('#lastName');
-            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+function emailValidation() {
+    $(document).ready(function () {
+        const emailInput = $('#email');
+        const emailError = $('#emailError');
+        const addToCartButton = $('#addToCart');
+        const firstNameInput = $('#firstName');
+        const lastNameInput = $('#lastName');
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        emailError.hide();
+        addToCartButton.prop('disabled', true); // Disable button initially
+
+        $('#chooseProductModal').on('show.bs.modal', function () {
+            addToCartButton.prop('disabled', true);
+            emailInput.val('');
+            firstNameInput.val('');
+            lastNameInput.val('');
             emailError.hide();
-            addToCartButton.prop('disabled', true); // Disable button initially
-
-            $('#chooseProductModal').on('show.bs.modal', function () {
-                addToCartButton.prop('disabled', true);
-                emailInput.val('');
-                firstNameInput.val('');
-                lastNameInput.val('');
-                emailError.hide();
-            });
-
-            function validateInputs() {
-                if (
-                    firstNameInput.val().trim() !== '' &&
-                    lastNameInput.val().trim() !== '' &&
-                    emailRegex.test(emailInput.val())
-                ) {
-                    addToCartButton.prop('disabled', false);
-                } else {
-                    addToCartButton.prop('disabled', true);
-                }
-            }
-
-            firstNameInput.on('input', validateInputs);
-            lastNameInput.on('input', validateInputs);
-            emailInput.on('input', function() {
-
-                emailError.toggle(!emailRegex.test(emailInput.val()));
-                validateInputs();
-            });
-
-            $('body').on('click', '#addToCart', function (e) {
-                e.preventDefault();
-
-                if (emailRegex.test(emailInput.val())) {
-                    console.log('Valid email:', emailInput.val());
-
-                }
-            });
         });
+
+        function validateInputs() {
+            if (
+                firstNameInput.val().trim() !== '' &&
+                lastNameInput.val().trim() !== '' &&
+                emailRegex.test(emailInput.val())
+            ) {
+                addToCartButton.prop('disabled', false);
+            } else {
+                addToCartButton.prop('disabled', true);
+            }
+        }
+
+        firstNameInput.on('input', validateInputs);
+        lastNameInput.on('input', validateInputs);
+        emailInput.on('input', function () {
+            emailError.toggle(!emailRegex.test(emailInput.val()));
+            validateInputs();
+        });
+
+        $('body').on('click', '#addToCart', function (e) {
+            e.preventDefault();
+
+            if (emailRegex.test(emailInput.val())) {
+                console.log('Valid email:', emailInput.val());
+            }
+        });
+    });
 }
 
-base.addToCart= base1.addToCart;
-base.updateGiftCerticate=updateGiftCerticate;
-base.closeModal=closeModal;
-base.updateAddToCart=updateAddToCart;
-base.emailValidation=emailValidation;
-module.exports=base;
+base.updateGiftCerticate = updateGiftCerticate;
+base.closeModal = closeModal;
+base.updateAddToCart = updateAddToCart;
+base.emailValidation = emailValidation;
 
-
+module.exports = base;
