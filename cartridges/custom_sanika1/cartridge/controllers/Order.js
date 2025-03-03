88a5ceb0b6a95afb180d8583641064bac76ca81c
SanikaWadake <sanika.wadake@systems-plus.com>
2025-03-03 12:25:10 +0530

    SFRA-5-Changes

--- cartridges/custom_sanika1/cartridge/controllers/Order.js
+++ cartridges/custom_sanika1/cartridge/controllers/Order.js
@@ -1,6 +1,4 @@
 
-
-
 'use strict';
 
 /**
@@ -8,79 +6,16 @@
  */
 
 var server = require('server');
+server.extend(module.superModule);
 
 var Resource = require('dw/web/Resource');
 var URLUtils = require('dw/web/URLUtils');
-var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
-var userLoggedIn = require('*/cartridge/scripts/middleware/userLoggedIn');
-var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
 
-
-server.extend(module.superModule);
-
-/**
- * Order-Details : This endpoint is called to get Order Details
- * @name Base/Order-Details
- * @function
- * @memberof Order
- * @param {middleware} - consentTracking.consent
- * @param {middleware} - server.middleware.https
- * @param {middleware} - userLoggedIn.validateLoggedIn
- * @param {querystringparameter} - orderID - Order ID
- * @param {querystringparameter} - orderFilter - Order Filter ID
- * @param {category} - sensitive
- * @param {serverfunction} - get
- */
-server.replace(
-    'Details',
-    consentTracking.consent,
-    server.middleware.https,
-    userLoggedIn.validateLoggedIn,
-    function (req, res, next) {
+server.append('Confirm',function (req, res, next) {
         var OrderMgr = require('dw/order/OrderMgr');
-        var orderHelpers = require('*/cartridge/scripts/order/orderHelpers');
-
-        var order = OrderMgr.getOrder(req.querystring.orderID);
-        var orderCustomerNo = req.currentCustomer.profile.customerNo;
-        var currentCustomerNo = order.customer.profile.customerNo;
-        var breadcrumbs = [
-            {
-                htmlValue: Resource.msg('global.home', 'common', null),
-                url: URLUtils.home().toString()
-            },
-            {
-                htmlValue: Resource.msg('page.title.myaccount', 'account', null),
-                url: URLUtils.url('Account-Show').toString()
-            },
-            {
-                htmlValue: Resource.msg('label.orderhistory', 'account', null),
-                url: URLUtils.url('Order-History').toString()
-            }
-        ];
-
-        if (order && orderCustomerNo === currentCustomerNo) {
-            var orderModel = orderHelpers.getOrderDetails(req);
-            var exitLinkText = Resource.msg('link.orderdetails.orderhistory', 'account', null);
-            var exitLinkUrl = URLUtils.https('Order-History', 'orderFilter', req.querystring.orderFilter);
-            res.setViewData({ pageID: 'Order-Details' });
-            res.render('account/orderDetails', {
-                order: orderModel,
-                exitLinkText: exitLinkText,
-                exitLinkUrl: exitLinkUrl,
-                breadcrumbs: breadcrumbs
-            });
-        } else {
-            res.redirect(URLUtils.url('Account-Show'));
-        }
-        next();
-    }
-);
-server.append(
-    'Confirm',
-    consentTracking.consent,
-    server.middleware.https,
-    csrfProtection.generateToken,
-    function (req, res, next) {
+        var Locale = require('dw/util/Locale');
+        var Site = require('dw/system/Site');
+        var Transaction = require('dw/system/Transaction');
         var reportingUrlsHelper = require('*/cartridge/scripts/reportingUrls');
         var OrderModel = require('*/cartridge/models/order');
 
@@ -131,7 +66,7 @@ server.append(
             //Rewards points
             var rewardPointsToAmount = Site.getCurrent().getCustomPreferenceValue('rewardPointsToAmount') || 10;
             var orderTotal = order.totalGrossPrice.value;
-            var earnedPoints = Math.floor(orderTotal / rewardPointsToAmount);
+            var earnedPoints = Math.floor(orderTotal / rewardPointsToAmount);//60/10
             var currentPoints=0;
             Transaction.wrap(function () {
                 order.custom.rewardPoints = earnedPoints;
@@ -152,4 +87,5 @@ server.append(
         return next();
     }
 );
+
 module.exports = server.exports();
