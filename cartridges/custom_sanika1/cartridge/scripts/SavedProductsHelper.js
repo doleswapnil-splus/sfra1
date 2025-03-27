e6928fe8fa09f6e673da4b165eb2063bfc240f7d
SanikaWadake <sanika.wadake@systems-plus.com>
2025-03-27 11:08:28 +0530

    SFRA-6-ScriptChange

--- cartridges/custom_sanika1/cartridge/scripts/SavedProductsHelper.js
+++ cartridges/custom_sanika1/cartridge/scripts/SavedProductsHelper.js
@@ -4,80 +4,11 @@ var collections = require('*/cartridge/scripts/util/collections');
 var BasketMgr = require('dw/order/BasketMgr');
 var Transaction = require('dw/system/Transaction');
 var CustomObjectMgr = require('dw/object/CustomObjectMgr');
-
-// function saveForLater(basket, productId) {
-//     var items = basket.getProductLineItems();
-//     var itemToSave;
-
-// collections.forEach(items, function (item) {
-//     if (item.product.ID === productId) {
-//         itemToSave = item;
-//     }
-// });
-
-// if (itemToSave) {
-//     Transaction.wrap(function () {
-
-//         var savedItem = CustomObjectMgr.getCustomObject('SavedForLater', productId);
-//         if (!savedItem) {
-//             savedItem = CustomObjectMgr.createCustomObject('SavedForLater', productId);
-//         }
-
-//         savedItem.custom.ProductId = productId;
-//         savedItem.custom.name = itemToSave.product.name;
-//         savedItem.custom.price = itemToSave.product.getPriceModel().getPrice().getValue();
-//         savedItem.custom.image = itemToSave.product.getImage('small').getURL();
-//         savedItem.custom.quantity = itemToSave.quantityValue;
-//         savedItem.custom.uuId = itemToSave.UUID;
-
-
-//     var variantAttributes = [];
-//     var variationModel = itemToSave.product.variationModel;
-
-//     if (variationModel) {
-//         var attributes = variationModel.productVariationAttributes; // Get all variation attributes
-//         collections.forEach(attributes, function (attribute) {
-//             var selectedValue = variationModel.getSelectedValue(attribute);
-//             if (selectedValue) {
-//                 variantAttributes.push({
-//                     displayName: attribute.displayName,
-//                     selectedValue: selectedValue.displayValue
-//                 });
-//             }
-//         });
-//     }
-//     savedItem.custom.options = JSON.stringify(variantAttributes);
-//         var b =itemToSave.custom.isGiftCertificate;
-
-//     if (itemToSave.custom.isGiftCertificate && itemToSave.custom.giftCertificateType==='email') {
-//         savedItem.custom.isGiftCertificate = itemToSave.custom.isGiftCertificate;
-//         savedItem.custom.giftCertificateType = itemToSave.custom.giftCertificateType;
-//         savedItem.custom.firstName = itemToSave.custom.firstName;
-//         savedItem.custom.lastName = itemToSave.custom.lastName;
-//         savedItem.custom.email = itemToSave.custom.email;
-//     } else {
-//         savedItem.custom.isGiftCertificate = false;
-//     }
-
-//     basket.removeProductLineItem(itemToSave);
-
-//     });
-//     return itemToSave;
-// }
-//   return null;
-// }
-
-
-
-var collections = require('dw/util/Collection');
 var ProductListMgr = require('dw/customer/ProductListMgr');
 var ProductList = require('dw/customer/ProductList');
-var Transaction = require('dw/system/Transaction');
 var ProductMgr = require('dw/catalog/ProductMgr');
 
-
 function saveForLater(basket, customer, productId, isGiftCertificate, giftCertificateType, firstName, lastName, email) {
-
     var items = basket.getProductLineItems().toArray();
     var itemToSave = null;
     var product = ProductMgr.getProduct(productId);
