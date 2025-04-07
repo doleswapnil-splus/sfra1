d0102abbe2e879c7108145dc0af4a2edf30b50d9
SanikaWadake <sanika.wadake@systems-plus.com>
2025-04-07 11:37:06 +0530

    SFRA-7-CodeChanges

--- cartridges/custom_sanika1/cartridge/scripts/jobs/exportFeed.js
+++ cartridges/custom_sanika1/cartridge/scripts/jobs/exportFeed.js
@@ -1,80 +1,91 @@
 
 'use strict';
 
-var Logger = require('dw/system/Logger');
 var File = require('dw/io/File');
 var FileWriter = require('dw/io/FileWriter');
 var XMLStreamWriter = require('dw/io/XMLStreamWriter');
 var ProductMgr = require('dw/catalog/ProductMgr');
 var ProductRatingService = require('*/cartridge/scripts/services/ProductRatingService');
-var StringWriter = require('dw/io/StringWriter');
+var Resource = require('dw/web/Resource');
 
-function getAllProducts(parameters, stepExecution) {
+function getProductRating() {
     var productIterator = ProductMgr.queryAllSiteProducts();
-    return productIterator.hasNext() ? productIterator.next() : null;
-}
+    var allReviews = [];
+    var noRating = Resource.msg('service.no.rating', 'service', null);
 
-function getProductRating(product) {
-    var ratingResponse = ProductRatingService.productService();
+    while (productIterator.hasNext()) {
+        var product = productIterator.next();
+        var productId = product.ID;
+        var ratingResponse = ProductRatingService.productService(productId);
 
-    if (!ratingResponse.ok) {
-        return;
+        if (!ratingResponse.ok) {
+            continue;
+        }
+
+        var responseObject = ratingResponse.object;//ret strg
+        var items = JSON.parse(responseObject);//con to json obj
+
+        if (items.message && items.message.indexOf(noRating) !== -1) {
+            continue;
+        }
+
+        delete items.action;
+        delete items.locale;
+        delete items.queryString;
+
+        var reviews = Object.values(items);
+        if (reviews.length === 0) {
+            continue;
+        }
+
+        allReviews = allReviews.concat(reviews);
     }
 
-    var responseObject = ratingResponse.object;
+    productIterator.close();
 
-    var b =JSON.stringify(responseObject);
-    
-    var reviews = Object.keys(responseObject)
-        .filter(key => !isNaN(key))
-        .map(key => responseObject[key]);
+    if (allReviews.length > 0) {
+        var path = File.IMPEX + File.SEPARATOR + "src" + File.SEPARATOR + "export" + File.SEPARATOR + 'exportRating.xml';
+        var file = new File(path);
+        var fileWriter = new FileWriter(file);
+        var xmlWriter = new XMLStreamWriter(fileWriter);
 
-    var path = File.IMPEX + File.SEPARATOR + "src" + File.SEPARATOR + "export" + File.SEPARATOR + 'exportRating.xml';
+        xmlWriter.writeStartDocument();
+        xmlWriter.writeStartElement('Reviews');
 
-    var file = new File(path);
-    var fileWriter = new FileWriter(file);
-    var xmlWriter = new XMLStreamWriter(fileWriter);
+        allReviews.forEach(function (review) {
+            xmlWriter.writeStartElement("Review");
 
-    xmlWriter.writeStartDocument();
-    xmlWriter.writeStartElement('Reviews');
+            xmlWriter.writeStartElement("orderId");
+            xmlWriter.writeCharacters(String(review.orderId || "N/A"));
+            xmlWriter.writeEndElement();
 
-    reviews.forEach(function (review) {
-        xmlWriter.writeStartElement("Review");
+            xmlWriter.writeStartElement("review");
+            xmlWriter.writeCharacters(review.review || "No comment");
+            xmlWriter.writeEndElement();
 
-        xmlWriter.writeStartElement("orderId");
-        xmlWriter.writeCharacters(String(review.orderId || "N/A"));
-        xmlWriter.writeEndElement();
+            xmlWriter.writeStartElement("Rating");
+            xmlWriter.writeCharacters(String(review.rating || 0));
+            xmlWriter.writeEndElement();
 
-        xmlWriter.writeStartElement("review");
-        xmlWriter.writeCharacters(review.review || "No comment");
-        xmlWriter.writeEndElement();
+            xmlWriter.writeStartElement("Reviewer");
+            xmlWriter.writeCharacters(review.email || "Anonymous");
+            xmlWriter.writeEndElement();
 
-        xmlWriter.writeStartElement("Rating");
-        xmlWriter.writeCharacters(String(review.rating || 0));
-        xmlWriter.writeEndElement();
-
-        xmlWriter.writeStartElement("Reviewer");
-        xmlWriter.writeCharacters(review.email || "Anonymous");
-        xmlWriter.writeEndElement();
+            xmlWriter.writeEndElement();
+        });
 
         xmlWriter.writeEndElement();
-    });
+        xmlWriter.writeEndDocument();
 
-    xmlWriter.writeEndElement();
-    xmlWriter.writeEndDocument();
-
-    xmlWriter.close();
-    fileWriter.close();
-}
-
-function after(success, parameters, stepExecution) {
-    if (success) {
-        Logger.info('Job Completed Successfully.');
-    } else {
-        Logger.error('Job Failed.');
+        xmlWriter.close();
+        fileWriter.close();
     }
 }
 
 module.exports = {
     getProductRating: getProductRating
 };
+
+
+
+
--- cartridges/custom_sanika1/cartridge/scripts/services/ProductRatingService.js
+++ cartridges/custom_sanika1/cartridge/scripts/services/ProductRatingService.js
@@ -1,40 +1,22 @@
 'use strict';
 
 var LocalServiceRegistry = require('dw/svc/LocalServiceRegistry');
-var CustomObjectMgr = require('dw/object/CustomObjectMgr');
 
-function productService() {
+function productService(productID) {
     var ProductService = LocalServiceRegistry.createService('ProductService', {
         createRequest: function (svc, productID) {
             svc.setRequestMethod('GET');
             svc.addHeader('Content-Type', 'application/json');
+            svc.setURL(svc.getConfiguration().credential.URL + '?productId=' + productID);
             return svc;
         },
         parseResponse: function (svc, response) {
             return response.text;
         }
     });
-    return ProductService.call();
+
+    return ProductService.call(productID);
 }
-
-/*function getProductDetailsWithReviews(productId) {
-    var productResponse = ProductService.call(productId);
-
-    var productData = productResponse.object;
-
-    var ratingObject = CustomObjectMgr.getCustomObject('Ratings_And_Reviews', productId);
-    var reviews = ratingObject ? JSON.parse(ratingObject.custom.Review || '[]') : [];
-
-    return {
-        ok: true,
-        object: {
-            id: productData.id,
-            name: productData.name,
-            price: productData.price || 'N/A',
-            reviews: reviews
-        }
-    };
-}*/
 
 module.exports = {
     productService: productService
